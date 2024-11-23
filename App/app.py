25e66a2a72424d17cf40dbca814cfcba46b3f656
CharloZeBerlingo <capprovencher@icloud.com>
2024-11-23 16:04:59 -0500

    h

--- App/app.py
+++ App/app.py
@@ -151,6 +151,7 @@ def grid_area(num_resistors, num_bulbs, num_switch):
     clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
